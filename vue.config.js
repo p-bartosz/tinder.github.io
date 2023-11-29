1fd7d2c70bf84933bc2ab059ef93e7a77ef2a186
Bartosz Picheta <bartoszpicheta@gmail.com>
2023-11-29 15:27:33 +0100

    created deploy.yml

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV==="production"?"/dist/":"/",
-  pwa: {
-    iconPaths: {
-      
-    }
-  }
+  publicPath: process.env.NODE_ENV === "production" ? "/tinder.github.io/" : "/",
 })
